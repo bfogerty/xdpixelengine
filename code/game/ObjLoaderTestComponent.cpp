f5fd57e1878941133634db3316a8011bad72e1f3
bfogerty <bfogerty@gmail.com>
2013-12-09 18:39:48 -0800

    Demonstrates loading an OBJ with textures

--- code/game/ObjLoaderTestComponent.cpp
+++ code/game/ObjLoaderTestComponent.cpp
@@ -11,24 +11,25 @@
 #include "../core/math/Vector3.h"
 #include "../core/math/Quaternion.h"
 #include "../core/math/MathHelper.h"
-
+#include "../renderer/Material.h"
 
 //-----------------------------------------------------------------------------------
 void ObjLoaderTestComponent::OnAwake()
 {
+
+	this->mpGameObject->pTexture = ImageLoader::Load("./assets/textures/blade_blue.png");
+
 	Mesh *mesh = mpGameObject->mMesh;
 
-	Mesh *pMeshTest = ObjLoader::Load("./assets/models/man.obj");
+	Mesh *pMeshTest = ObjLoader::Load("./assets/models/tekka.obj");
+	//Mesh *pMeshTest = GameObject::CreatePrimitive( GameObject::EPrimitiveType::Torus )->mMesh;
 	mesh->Clone(pMeshTest);
 
 	mft = 0.0f;
 
 	fX = 0.0f;
-	fY = 0.0f;
+	fY = 0.15f;
 	fZ = 1.0f;
-
-	mpSphere = GameObject::CreatePrimitive(GameObject::Plane);
-	mpSphere->mpTransform->SetParent(*mpGameObject->mpTransform);
 }
 
 
@@ -46,43 +47,5 @@ void ObjLoaderTestComponent::OnUpdate()
 	this->mpGameObject->mpTransform->Rotation = Quaternion::AxisAngle(Vector3::Up(), fDegrees);
 	this->mpGameObject->mpTransform->Position = Vector3(fX,fY,fZ);
 
-	this->mpSphere->mpTransform->Scale = Vector3(0.5f,0.5f,0.5f);
-	this->mpSphere->mpTransform->Rotation = Quaternion::AxisAngle(Vector3::Up(), fDegrees);
-	this->mpSphere->mpTransform->Position = Vector3(fX,fY,fZ);
-
 	mft += Time::GetInstance()->GetDeltaTime() / 5;
-
-	/*
-	if( Input::Inst()->GeyKey(KeyCode::LeftArrow) ||
-		Input::Inst()->GeyKey(KeyCode::A) )
-	{
-		fX -= (1.f * Time::GetInstance()->GetDeltaTime());
-	}
-
-	if( Input::Inst()->GeyKey(KeyCode::RightArrow) ||
-		Input::Inst()->GeyKey(KeyCode::D))
-	{
-		fX += (1.f * Time::GetInstance()->GetDeltaTime());
-	}
-
-	if( Input::Inst()->GeyKey(KeyCode::UpArrow) )
-	{
-		fY += (1.f * Time::GetInstance()->GetDeltaTime());
-	}
-
-	if( Input::Inst()->GeyKey(KeyCode::DownArrow) )
-	{
-		fY -= (1.f * Time::GetInstance()->GetDeltaTime());
-	}
-
-	if( Input::Inst()->GeyKey(KeyCode::W) )
-	{
-		fZ += (1.f * Time::GetInstance()->GetDeltaTime());
-	}
-
-	if( Input::Inst()->GeyKey(KeyCode::S) )
-	{
-		fZ -= (1.f * Time::GetInstance()->GetDeltaTime());
-	}
-	*/
 }
